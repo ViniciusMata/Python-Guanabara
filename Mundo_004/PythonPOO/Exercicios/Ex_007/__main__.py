6d97846af6dc2235f95db27e09e8318039a25bf7
ViniciusMata <damata.vini@gmail.com>
2026-09-02 10:42:54 +0100

    feat: adicionado Aulas/Exercicios Mundo_004

--- Mundo_004/PythonPOO/Exercicios/Ex_007/__main__.py
+++ Mundo_004/PythonPOO/Exercicios/Ex_007/__main__.py
@@ -1,23 +1,24 @@
 from rich import print, inspect
-from aluno import Aluno
-from professor import Professor
-from funcionario import Funcionario 
+from classes import Pessoa, Aluno, Professor, Funcionario 
 
 def main():
     a1 = Aluno("José", 17, "Informática", "T01")
     a1.fazer_Aniversario()
     a1.fazerMatricula()
-    inspect(a1, methods=True)
+    a1.estudar()
+    #inspect(a1, methods=True)
 
     p1 = Professor("Samuel", 37, "Biologia", "Mestrado")
     p1.fazer_Aniversario()
     p1.dar_aula()
-    inspect(p1, methods=True)
+    p1.estudar()
+    #inspect(p1, methods=True)
 
     f1 = Funcionario("Cláudia", 27, "Secretaria", "Secretaria")
     f1.fazer_Aniversario()
     f1.bater_ponto()
-    inspect(f1, methods=True)
+    f1.estudar()
+    #inspect(f1, methods=True)
     
 if __name__ == "__main__":
     main()
